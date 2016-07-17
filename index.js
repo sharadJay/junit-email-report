cc4401b913202dd40bf92b60caffdc23642b0e7d
Sharad Jain <shjain@expedia.com>
2016-07-17 10:44:24 +0530

    initial working solution

---
+++ index.js
@@ -0,0 +1,18 @@
+var merger = require('junit-report-merger');
+var glob = require('glob-fs')({gitignore: true});
+var parser = require('junit-xml-parser').parser;
+fs = require('fs');
+var swig = require('swig');
+var template = swig.compileFile('emailTemplate.html');
+
+glob.readdir('reports/**/*.xml', function (err, files) {
+    merger.mergeFiles("/Users/shjain/junit-email-report/results/result.xml", files, [], function (res, err) {
+        fs.readFile('/Users/shjain/junit-email-report/results/result.xml', 'utf8', function (err, data) {
+            parser.parse(data).then(function (results) {
+                var output = template(results);
+                console.log(output);
+            });
+        });
+    });
+});
+
